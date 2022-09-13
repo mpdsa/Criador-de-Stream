08132ee1f8b37b454b23c7c83829e63715edf92a
marcos <marcos@li0001>
2022-09-13 18:28:29 -0300

    Initial commit.

--- src/main/java/com/trybe/acc/java/CriadorStream.java
+++ src/main/java/com/trybe/acc/java/CriadorStream.java
@@ -1,5 +1,35 @@
 package com.trybe.acc.java;
 
-public class CriadorStream {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
+/** Classe CriadorStream. */
+public class CriadorStream {
+  
+  /**
+   * Método criarDoItem.
+   * Fonte https://www.oracle.com/br/technical-resources/articles/java-stream-api.html.
+   */
+  public <T> Stream<T> criarDoItem(T item) {
+    List<T> items = new ArrayList<T>();
+    items.add(item);
+    
+    return items.stream();
+  }
+  
+  
+  /**
+   * Fonte https://mkyong.com/java8/java-how-to-convert-array-to-stream/
+   */
+  public <T> Stream<T> criarDoArray(T[] array) {
+    return Arrays.stream(array);
+    
+  }
+  
+  public <T> Stream<T> criarDaLista(List<T> lista) {
+    return lista.stream();
+    
+  }
 }
